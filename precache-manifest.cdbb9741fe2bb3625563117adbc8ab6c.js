a9b242b4aba1028f8248b11131a404b2fc80535d
Kevlar2222 <kevin.satti@hotmail.co.uk>
2020-09-18 17:18:27 +0800

    Updates

---
+++ precache-manifest.cdbb9741fe2bb3625563117adbc8ab6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "677f7b34776011a607a527db75d9764b",
+    "url": "/tnw/index.html"
+  },
+  {
+    "revision": "12609f6ec8221c2da2f8",
+    "url": "/tnw/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34f67b59d4901e5828e1",
+    "url": "/tnw/static/js/2.24fdd6f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tnw/static/js/2.24fdd6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12609f6ec8221c2da2f8",
+    "url": "/tnw/static/js/main.4c221479.chunk.js"
+  },
+  {
+    "revision": "50363e68b30f358b64c1",
+    "url": "/tnw/static/js/runtime-main.d6de3969.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tnw/static/media/logo.5d5d9eef.svg"
+  }
+]);
